426ea13c1aab5c05c40e0375be353c27be52490c
Alan Lima <alima@outlook.com>
2026-09-07 06:39:55 +1000

    Add organisation-scoped native administration for #10

--- signal_loop/urls.py
+++ signal_loop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from signal_loop.accounts.views import SignInView, SignOutView, application_entry
+from signal_loop.membership.admin import site as organisation_admin
 from signal_loop.views import home
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path("accounts/login/", SignInView.as_view(), name="login"),
     path("accounts/logout/", SignOutView.as_view(), name="logout"),
     path("app/", application_entry, name="application_entry"),
+    path("admin/", organisation_admin.urls),
 ]
